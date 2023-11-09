78dffb248f2a263fef01287293bcaa21aa5ebc44
Yuriy Maksyuta <yura-maksuta@mail.ru>
2023-11-09 20:42:49 +0300

    Fix metrics

--- hw_ss/metric/SiSdrMetric.py
+++ hw_ss/metric/SiSdrMetric.py
@@ -1,4 +1,4 @@
-from torchmetrics.audio import ScaleInvariantSignalDistortionRatio
+from torchmetrics.functional.audio import scale_invariant_signal_distortion_ratio
 from hw_ss.base.base_metric import BaseMetric
 import torch
 
@@ -6,10 +6,8 @@ import torch
 class SiSdrMetric(BaseMetric):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.si_sdr = ScaleInvariantSignalDistortionRatio()
 
     def __call__(self, s1, s2, s3, audio_tgt, *args, **kwargs):
         with torch.no_grad():
             pred_audio = 0.8 * s1 + 0.1 * s2 + 0.1 * s3
-            print(pred_audio.device, audio_tgt.device)
-            return self.si_sdr(pred_audio, audio_tgt.squeeze(1))
+            return scale_invariant_signal_distortion_ratio(pred_audio, audio_tgt.squeeze(1))
